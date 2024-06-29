f1d3903d2f95e7d4557331d7f22cac8c3aceef52
GED <gimaev.ufa@gmail.com>
2024-06-29 13:12:19 +0500

    feat: refactoring for AnswersController get #create

--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -4,25 +4,11 @@ RSpec.describe AnswersController, type: :controller do
 
   let(:question) { create(:question) }
   let(:answer) { create(:answer, question: question) }
-  # let(:answers) { create_list(:answer, 3, question: question) }
-  
-  # describe 'GET #index' do 
-  #   let(:question) { create(:question) }
-  #   let(:answers) { create_list(:answer, 3, question: question) }
-
-  #   before {get :index, params: { id: question }}
-    
-  #   it 'Populates an array of all answers' do 
-  #     expect(assigns(:answers)).to match_array(@answers)
-  #   end
-
-  #   it 'Renders index view' do 
-  #     expect(response).to render_template :index
-  #   end
-  # end
 
   describe 'POST #create' do
+
     context 'with valid attributes' do
+
       it 'saves a new answer in the database' do
         expect do
           post :create, 
@@ -31,9 +17,11 @@ RSpec.describe AnswersController, type: :controller do
                                       question_id: question.id)}
         end.to change(Answer, :count).by(1)
       end
+
     end
 
     context 'with invalid attributes' do
+
       it 'does not save a answer in DB' do
         expect do
           post :create, 
@@ -43,7 +31,9 @@ RSpec.describe AnswersController, type: :controller do
                                                 question_id: question.id)}
         end.not_to change(Answer, :count)
       end
+      
     end
+
   end
 
 end
