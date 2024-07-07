fa2055859b76281a94138cf2079fe1786958883b
GED <gimaev.ufa@gmail.com>
2024-07-07 13:12:10 +0500

    feat: added tests for Authenticated/Unauthenticated user can view a list of questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_03_173152) do
+ActiveRecord::Schema.define(version: 2024_07_05_145438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2024_07_03_173152) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 2024_07_03_173152) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "users"
 end
