2709003130dafcfe36224945e246a7ac9d9c4cb9
GED <gimaev.ufa@gmail.com>
2024-07-17 22:07:37 +0200

    feat: added test for signing out

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to terminate the current session
+  as an authenticated user
+  I would like be able to sign out
+} do 
+  given(:user) {create(:user)}
+
+  scenario 'Authenticated user tries to sign out' do 
+    sign_in(user)
+
+    click_on 'Log Out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+
+end
