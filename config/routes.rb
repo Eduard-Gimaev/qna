bdf3a37e5479f601ab9622c1099ca480de4016ff
GED <gimaev.ufa@gmail.com>
2024-07-28 12:26:35 +0200

    fix: webpacker has been reinstalles and Compiled successfully

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :questions, shallow: true do
-    resources :answers, shallow: true
+    resources :answers, shallow: true, only: %i[create destroy]
   end
 
   root to: 'questions#index'
