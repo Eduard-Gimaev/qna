3d3fe054708eeec4a453264fa8fc559dc7487010
GED <gimaev.ufa@gmail.com>
2024-08-03 22:18:45 +0200

    feat: added test should have a correct author

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should have_many(:questions).dependent(true) }
   it { should have_many(:answers).dependent(true) }
+
+  let(:user1) { create(:user) }
+  let(:question1) { create(:question, user: user1) }
+  let(:answer1) { create(:answer, question: question1, user: user1) }
+
+  let(:user2) { create(:user) }
+  let(:question2) { create(:question, user: user2) }
+  let(:answer2) { create(:answer, question: question2, user: user2) }
+
+  it 'ahould have a correct author' do
+    expect(user1).to be_author(question1)
+    expect(user1).to be_author(answer1)
+    expect(user2).to be_author(question2)
+    expect(user2).to be_author(answer2)
+
+
+  end
 end
