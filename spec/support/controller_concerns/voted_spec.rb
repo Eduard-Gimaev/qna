3bd4e6dba9e05823e6fece059b7cd00defd7b28a
GED <gimaev.ufa@gmail.com>
2024-09-12 16:08:31 +0100

    feat: added tests for controller_concerns - voted

--- spec/support/controller_concerns/voted_spec.rb
+++ spec/support/controller_concerns/voted_spec.rb
@@ -3,17 +3,39 @@ require 'rails_helper'
 shared_examples 'voted' do
   let!(:author) { create(:user) }
   let!(:resource) { create(described_class.name.underscore.split("_")[0][0..-2].to_sym, user: author) }
+  let!(:user) { create(:user) }
   
-
   describe 'PATCH #like' do
-  before do
+    before do
       login(user)
       post :like, params: { id: resource.id }, format: :json
     end
-  end
 
-  it 'creates new like' do
+    it 'creats a vote(like)' do
       expect(resource.votes[0].votable).to eq resource
     end
 
+    it 'cancels a created vote(like)' do
+      post :like, params: { id: resource.id }, format: :json
+
+      expect(resource.votes.count).to eq 0
+    end
+  end
+
+  describe 'PATCH #dislike' do
+    before do
+      login(user)
+      post :dislike, params: { id: resource.id }, format: :json
+    end
+
+    it 'creats a vote(dislike)' do
+      expect(resource.votes[0].votable).to eq resource
+    end
+
+    it 'cancels a created vote(dislike)' do
+      post :dislike, params: { id: resource.id }, format: :json
+
+      expect(resource.votes.count).to eq 0
+    end
+  end
 end
