914a0dbdc338a02159c46c293bcf893c3ad8adeb
GED <gimaev.ufa@gmail.com>
2024-10-09 13:32:41 +0200

    fix: removed redundant tests from voted_spec for controllers

--- spec/support/controller_concerns/voted_spec.rb
+++ spec/support/controller_concerns/voted_spec.rb
@@ -10,15 +10,15 @@ RSpec.shared_examples 'voted' do
       login(user)
       post :like, params: { id: resource.id }, format: :json
     end
+    context 'when user is not the author of the entity' do
+      it 'creates a vote(like)' do
+        expect(resource.votes.count).to eq 1 
+      end
 
-    it 'creats a vote(like)' do
-      expect(resource.votes[0].votable).to eq resource
-    end
-
-    it 'cancels a created vote(like)' do
-      post :like, params: { id: resource.id }, format: :json
-
-      expect(resource.votes.count).to eq 0
+      it 'cancels a created vote(like)' do
+        post :like, params: { id: resource.id }, format: :json
+        expect(resource.votes.count).to eq 0
+      end
     end
   end
 
@@ -27,32 +27,14 @@ RSpec.shared_examples 'voted' do
       login(user)
       post :dislike, params: { id: resource.id }, format: :json
     end
-
-    it 'creats a vote(dislike)' do
-      expect(resource.votes[0].votable).to eq resource
-    end
-
-    it 'cancels a created vote(dislike)' do
-      post :dislike, params: { id: resource.id }, format: :json
-      expect(resource.votes.count).to eq 0
-    end
-  end
-
-  describe '#prepare_vote' do
-    context 'when user is the author of the entity' do
-      it 'renders an error message' do
-        login(user)
-        post :like, params: { id: resource.id, format: :json }
-        expect(response.body).to include("You can't vote for your own #{resource.class.name.underscore}")
-      end
-    end
-
     context 'when user is not the author of the entity' do
-      it 'renders the entity rating' do
-        login(another_user)
-        allow(resource).to receive(:make_vote).with(another_user, 'like')
-        post :like, params: { id: resource.id, format: :json }
-        expect(response.body).to eq('1')
+      it 'creates a vote(dislike)' do
+        expect(resource.votes.count).to eq 1
+      end
+
+      it 'cancels a created vote(dislike)' do
+        post :dislike, params: { id: resource.id }, format: :json
+        expect(resource.votes.count).to eq 0
       end
     end
   end
