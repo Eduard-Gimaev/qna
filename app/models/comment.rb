60650e3279001ff6cb957d8c226c741b475b66b4
GED <gimaev.ufa@gmail.com>
2025-03-08 17:22:01 +0100

    feat: added test for a model of comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: { minimum: 5 }
+  validates :user, presence: true
+  validates :commentable, presence: true
 end
