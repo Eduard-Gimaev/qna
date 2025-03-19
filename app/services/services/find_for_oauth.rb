5776ce89ccfb2b4507d3995b35542d6a91608803
GED <gimaev.ufa@gmail.com>
2025-03-20 00:28:41 +0100

    refactor

--- app/services/services/find_for_oauth.rb
+++ app/services/services/find_for_oauth.rb
@@ -7,26 +7,28 @@ module Services
     end
 
     def call
-      authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+      authorization = find_authorization
       return authorization.user if authorization
 
       email = auth.info[:email]
-      user = User.where(email: email).first
-      if user
-        user.authorizations.create(provider: auth.provider, uid: auth.uid)
-      elsif email
-        user = create_user_from_oauth(email, auth)
-      end
+      return nil unless email
+
+      user = find_or_create_user(email)
+      user.create_authorization(auth)
       user
     end
 
     private
 
-    def create_user_from_oauth(email, auth)
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-      user.authorizations.create(provider: auth.provider, uid: auth.uid)
-      user
+    def find_authorization
+      Authorization.find_by(provider: auth.provider, uid: auth.uid)
+    end
+
+    def find_or_create_user(email)
+      User.find_or_create_by(email: email) do |user|
+        user.password = Devise.friendly_token[0, 20]
+        user.password_confirmation = user.password
+      end
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,6 @@ module Qna
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.autoload_paths += Rails.root.glob('app/*')
-
     config.generators do |g|
       g.test_framework :rspec,
                        view_specs: false,
