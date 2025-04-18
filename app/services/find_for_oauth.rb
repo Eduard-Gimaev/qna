ba74bd04bfb273da9d6b6fedeb22aa4e4649203e
GED <gimaev.ufa@gmail.com>
2025-04-18 11:44:12 +0200

    fix: remove unnecessary methods in the find_for_oauth

--- app/services/find_for_oauth.rb
+++ app/services/find_for_oauth.rb
@@ -6,27 +6,18 @@ class FindForOauth
   end
 
   def call
-    authorization = find_authorization
+    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid)
     return authorization.user if authorization
 
     email = auth.info[:email]
     return nil unless email
 
-    user = initialize_user(email)
-    user.create_authorization(auth)
-    user
-  end
-
-  private
-
-  def find_authorization
-    Authorization.find_by(provider: auth.provider, uid: auth.uid)
-  end
-
-  def initialize_user(email)
-    User.find_or_create_by(email: email) do |user|
-      user.password = Devise.friendly_token[0, 20]
-      user.password_confirmation = user.password
+    user = User.find_or_create_by(email: email) do |a|
+      a.password = Devise.friendly_token[0, 20]
+      a.password_confirmation = a.password
     end
+
+    user.authorizations.create(provider: auth.provider, uid: auth.uid)
+    user
   end
 end
