bc1438b0dd32f72b2170e464f5fd9635a1cf546f
GED <gimaev.ufa@gmail.com>
2025-05-02 12:43:04 +0200

    Update logger configuration in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,13 +47,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-if ENV['RAILS_LOG_TO_STDOUT'].present?
   config.logger = Logger.new($stdout)
-  config.logger.formatter = config.log_formatter
-else
-  config.logger = Logger.new(Rails.root.join('log', "#{Rails.env}.log"))
-  config.logger.formatter = config.log_formatter
-end
+  config.logger.formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
