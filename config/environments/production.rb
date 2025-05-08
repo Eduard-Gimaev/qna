f2f9025ea3f8de29acafe3081e247738952be150
GED <gimaev.ufa@gmail.com>
2025-05-08 21:56:40 +0200

    Compile production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,7 @@ require 'logger'
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
@@ -16,16 +14,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,21 @@
 # frozen_string_literal: true
 
-# Количество потоков, которые Puma будет использовать
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
 threads min_threads_count, max_threads_count
 
-# Устанавливаем таймауты для работы воркеров (для девелопмент окружения)
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
-# Устанавливаем порт для Puma
 port ENV.fetch('PORT', 3000)
 
-# Устанавливаем окружение
 environment ENV.fetch('RAILS_ENV', 'development')
 
-# Устанавливаем pidfile
 pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
-# Позволяет Puma перезапускать приложение через команду `rails restart`
 plugin :tmp_restart
 
-# Указание пути до конфигурации приложения
 rackup "config.ru"
 
-# Параметры для логов и сокетов в продакшн
 if ENV.fetch('RAILS_ENV', 'development') == 'production'
   pidfile "/home/deployer/qna-app-server/shared/tmp/pids/puma.pid"
   state_path "/home/deployer/qna-app-server/shared/tmp/pids/puma.state"
@@ -31,9 +23,7 @@ if ENV.fetch('RAILS_ENV', 'development') == 'production'
   bind "unix:///home/deployer/qna-app-server/shared/tmp/sockets/puma.sock"
 end
 
-# Настройки для количества потоков и воркеров
 threads 1, 6
 workers 2
 
-# Прокачка Bundler
 prune_bundler
